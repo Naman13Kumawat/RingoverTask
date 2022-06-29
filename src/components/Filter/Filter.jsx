64eb10b066296b1df20c1aef35452d281d77c073
Naman13Kumawat <naman.kumawat07@gmail.com>
2022-06-29 17:31:59 +0530

    Add Navbar and Filter Component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,77 @@
-import './Filter.scss';
+import "./Filter.scss";
 
 export default function Filter() {
   return (
-    <div>Filter</div>
-  )
+    <div className="filter">
+      <span className="heading">
+        <h1>FILTERS</h1>
+        <img className="filterImg" src="/images/filter.png" alt="filter" />
+      </span>
+      <span className="cost">
+        <h2>Cost</h2>
+        <label>
+          <input type="checkbox" id="low" />
+          &nbsp; Rs. 1500-4000
+        </label>
+        <label>
+          <input type="checkbox" id="med" />
+          &nbsp; Rs. 4001-7000
+        </label>
+        <label>
+          <input type="checkbox" id="med" />
+          &nbsp; Rs. 7001+
+        </label>
+      </span>
+      <span className="color">
+        <h2>Color</h2>
+        <div className="color_box">
+          <label className="container">
+            <input type="checkbox" id="red" />
+            <span className="colorProps red"></span>
+          </label>
+          <label className="container">
+            <input type="checkbox" id="blue" />
+            <span className="colorProps blue"></span>
+          </label>
+          <label className="container">
+            <input type="checkbox" id="yellow" />
+            <span className="colorProps yellow"></span>
+          </label>
+          <label className="container">
+            <input type="checkbox" id="green" />
+            <span className="colorProps green"></span>
+          </label>
+          <label className="container">
+            <input type="checkbox" id="gold" />
+            <span className="colorProps gold"></span>
+          </label>
+        </div>
+      </span>
+      <span className="design">
+        <h2>Design Template</h2>
+        <label>
+          <input type="checkbox" id="two" />
+          &nbsp;2
+        </label>
+        <label>
+          <input type="checkbox" id="three" />
+          &nbsp;3
+        </label>
+        <label>
+          <input type="checkbox" id="more" />
+          &nbsp;3+
+        </label>
+      </span>
+      <span className="type">
+        <h2>Type</h2>
+        <label htmlFor="loafers">
+          <input type="checkbox" id="loafers" />&nbsp;Loafers
+        </label>
+        <label htmlFor="Sneakers">
+          <input type="checkbox" id="Sneakers" />&nbsp;Sneakers
+        </label>
+      </span>
+      <button className="btn">Apply</button>
+    </div>
+  );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,30 @@
 import "./Navbar.scss";
+import { findDOMNode } from "react-dom";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+
+  const handleClick = (e)=>{
+    console.log("Cliked", e.target.tagName);
+    // const elements = document.getElementsByClassName("other")
+    // elements.classList.remove("active");
+    e.target.classList.add("active");
+    
+    }
+
+  return ( 
+  <div className="navbar">
+    <img className="logo" src="/images/websitelogo.png" alt="logo" />
+    <ul>
+      <li onClick={handleClick} className="other">HOME</li>
+      <li onClick={handleClick} className="other">THE JOURNEY</li>
+      <li onClick={handleClick} className="other">TEAM</li>
+      <li onClick={handleClick} className="other active">STORE</li>
+      <li onClick={handleClick} className="other">CONTACT</li>
+    </ul>
+    <span className="profileDiv">
+      <img className="profile" src="/images/profile.png" alt="profile" />
+      <h1>GAGAN</h1>
+    </span>
+  </div>
+  );
 }
